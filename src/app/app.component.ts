9dc28e21ee7e05c730114252a208ac17cd6ace6b
Yulibeth Rivero <yulibeth.rivero@gmail.com>
2024-06-07 09:32:17 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
+import { ProductsService } from './api/products.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'angular';
+  private readonly productsSvc = inject(ProductsService);
+
+  // Creamos un observable 'products$'
+  products$ = this.productsSvc.getAllProducts();
 }
