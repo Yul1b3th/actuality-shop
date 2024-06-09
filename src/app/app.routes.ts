e6dc8dc1913ca944a51110c331d714ae1029d33b
Yulibeth Rivero <yulibeth.rivero@gmail.com>
2024-06-09 16:46:13 +0200

    Aplicar lazy loading a las rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,17 @@
 import { Routes } from '@angular/router';
-import { ProductsComponent } from './features/products/products.component';
-import { CartComponent } from './features/cart/cart.component';
-import { DetailsComponent } from './features/products/details/details.component';
 
 export const routes: Routes = [
-  {path: 'products', component: ProductsComponent},
-  {path: 'product-details/:id', component: DetailsComponent},
-  {path: 'cart', component: CartComponent},
+  {
+    path: 'products',
+    loadComponent: () => import('./features/products/products.component').then(c => c.ProductsComponent)
+  },
+  {
+    path: 'product-details/:id',
+    loadComponent: () => import('./features/products/details/details.component')
+  },
+  {
+    path: 'cart',
+    loadComponent: () => import('./features/cart/cart.component').then(c => c.CartComponent)
+  },
   {path: '', redirectTo: '/products', pathMatch: 'full'},
 ];
--- src/app/features/products/products.component.ts
+++ src/app/features/products/products.component.ts
@@ -1,6 +1,6 @@
-import { Component, inject } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, EnvironmentInjector, OnInit, inject, runInInjectionContext } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 import { ProductsService } from '../../api/products.service';
 import { CardComponent } from './card/card.component';
@@ -11,9 +11,23 @@ import { CardComponent } from './card/card.component';
   imports: [CommonModule, CardComponent],
   templateUrl: './products.component.html',
   styleUrl: './products.component.scss'
+
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit{
   productSvc = inject(ProductsService);
-  // Usamos product$ como un observable
+  // Usamos product$ como un observable para mostrar los productos
   products$ = this.productSvc.getAllProducts();
+  productSvc2: any;
+  private readonly injectorCtxt = inject(EnvironmentInjector);
+
+  ngOnInit(): void {
+    runInInjectionContext(this.injectorCtxt, () => {
+      this.productSvc2 = inject(ProductsService);
+      // Convertir un observable a toSignal
+      const result = toSignal(this.products$);
+      console.log(result);
+
+    });
+
+  }
 }
